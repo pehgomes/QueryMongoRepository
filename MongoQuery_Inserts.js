cbe9d9539b67d3386e8599a0ec920ac0b17f1a53
PedroHenriGomes <pedrogomesup2@gmail.com>
2017-01-22 13:30:45 -0300

    Inserts

--- MongoQuery_Inserts.js
+++ MongoQuery_Inserts.js
@@ -23,24 +23,27 @@ db.users.find({
     badges:  'black'  
         
 });
-
+/*
 buscando dentro de um array com mais especificaçoes
+*/
 db.users.find({});
 db.users.find({
     badges:  ['black','blue']  
         
 });
-
+/*
 Mais parametros de query, dessa vez buscando dentro de um Array('badges') 
 e tambem dentro de um objeto('Favorites')
+*/
 db.users.find({});
 db.users.find({
     badges:  ['black','blue'],
     'favorites.artist':'Picasso' 
         
 });
-
+/*
 Buscando dentro de um Objeto com $exists que recebe true ou false
+*/
 db.users.find({});
 db.users.find({
     'favorites.artist' : {
@@ -48,8 +51,9 @@ db.users.find({
     }
         
 });
-
+/*
 Limitando a quantidade de Fields que se deseja que apareça
+*/
 db.users.find({});
 db.users.find({
     'favorites.artist' : {
@@ -58,8 +62,10 @@ db.users.find({
         
 },{_id: 0, name: 1})
 
-
+/*
 Buscando com findOne
+*/
+
 db.users.find({});
 db.users.findOne({
     'favorites.artist' : {
